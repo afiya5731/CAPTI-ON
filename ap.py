a84845d021afb3d1594cb001586128e7c117f4fe
afiya5731 <162781027+afiya5731@users.noreply.github.com>
2025-11-14 08:13:51 +0530

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+import os
+from werkzeug.utils import secure_filename
+from model_runner import generate_caption
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+
+    file = request.files['image']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+
+    caption = generate_caption(filepath)
+    return jsonify({'caption': caption})
+
+if __name__ == '__main__':
+    app.run(debug=True)
